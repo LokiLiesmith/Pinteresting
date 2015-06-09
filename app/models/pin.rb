9a164d063230eb7d4eeaf0f4cd378a4c596f7ee2
Mark Ražem <mark.razem@gmail.com>
2015-06-09 18:11:23 +0200

    Add image upload with Paperclip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,13 +3,8 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, :styles => {:medium => "300x300>", :thumb => "100x100>"}
 	#validates :description, :presence => true
- 	
- 	#validates :user_id, :presence => true
- 	#validates_attachment :image, :presence => true
 
 # Validate content type
  validates_attachment_content_type :image, :content_type => /\Aimage/
-# Validate filename
- validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/]
-# Explicitly do not validate
+
 end
