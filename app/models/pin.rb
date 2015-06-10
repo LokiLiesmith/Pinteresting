aaa6f4e0cb483592b887978121c6b2b9ca347785
Mark Ražem <mark.razem@gmail.com>
2015-06-10 10:23:47 +0200

    added names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,8 +3,10 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, :styles => {:medium => "300x300>", :thumb => "100x100>"}
 	#validates :description, :presence => true
-
+	validates :description, presence: true
+	validates :image, presence: true
 # Validate content type
- validates_attachment_content_type :image, :content_type => /\Aimage/
+ 	validates_attachment_content_type :image, :content_type => /\Aimage/
+
 
 end
